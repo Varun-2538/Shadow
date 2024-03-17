feb5c9f71b73fa7720522af484a5e9ffe7826109
Varun singh <942525@gmail.com>
2024-03-17 23:56:42 +0530

    Over to ahaan

--- heatmap-frontend/src/App.js
+++ heatmap-frontend/src/App.js
@@ -1,11 +1,32 @@
-import React from 'react'
-import Map from './components/Map'
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import './App.css';
+import Home from './components/Home';
+import Dashboard from './components/Dashboard';
+import DataEntry from './components/DataEntry.js';
+import Map from './components/Map'; // Assuming your Map component is saved in a components folder
 
-export default function App() {
+const App = () => {
   return (
-    <div>
-      <Map />
-    </div>
-  )
+    <Router>
+      <div className="App">
+        <div className="sidebar">
+          <Link to="/">Analysis</Link>
+          <Link to="/dashboard">Prediction plan</Link>
+          <Link to="/data-entry">Deployment</Link>
+          <Link to="/map">Data Entry</Link>
+        </div>
+        <div className="main-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/data-entry" element={<DataEntry />} />
+            <Route path="/map" element={<Map />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
 }
+
+export default App;
