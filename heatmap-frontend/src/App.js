60315ab39275020abba1664188902ab455242085
Varun singh <942525@gmail.com>
2024-04-12 00:55:17 +0530

    Feat: added radio button

--- heatmap-frontend/src/App.js
+++ heatmap-frontend/src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
 
 import Deployment from './components/Deployment';
 import Spatial from './components/Spatial';
@@ -8,6 +8,8 @@ import Map from './components/Map';
 import Sidebar from './components/Sidebar';
 import Navbar from './components/Navbar';
 import Landing from './components/Landing';
+import BeatWiseAnalysis from './components/BeatWiseAnalysis';
+import TemporalAnalysis from './components/TemporalAnalysis';
 
 const AppWrapper = () => {
   return (
@@ -18,20 +20,57 @@ const AppWrapper = () => {
 };
 
 const App = () => {
-  const location = useLocation();
-  const isLandingPage = location.pathname === "/";
+  const [selectedPage, setSelectedPage] = useState('spatial'); // default page
+  const navigate = useNavigate();
+
+  const handleRadioChange = (event) => {
+    setSelectedPage(event.target.value);
+    navigate(`/${event.target.value}`);
+  };
 
   return (
     <div className="flex">
-      {!isLandingPage && <Sidebar />}
-      <div className={`flex-grow ${!isLandingPage ? 'ml-30' : 'w-full'}`}>
+      <Sidebar />
+      <div className="flex-grow ml-30">
         <Navbar />
         <div className="p-4">
+          <div className="radio-buttons">
+            <label>
+              <input
+                type="radio"
+                value="spatial"
+                checked={selectedPage === 'spatial'}
+                onChange={handleRadioChange}
+              />
+              Spatial Analysis
+            </label>
+            <label>
+              <input
+                type="radio"
+                value="beatwise"
+                checked={selectedPage === 'beatwise'}
+                onChange={handleRadioChange}
+              />
+              Beat Wise Analysis
+            </label>
+            <label>
+              <input
+                type="radio"
+                value="temporal"
+                checked={selectedPage === 'temporal'}
+                onChange={handleRadioChange}
+              />
+              Temporal Analysis
+            </label>
+            {/* Add more radio buttons if necessary */}
+          </div>
           <Routes>
             <Route path="/" element={<Landing />} />
-            <Route path="/Prediction" element={<Prediction />} />
-            <Route path="/Spatial" element={<Spatial />} />
-            <Route path="/Deployment" element={<Deployment />} />
+            <Route path="/prediction" element={<Prediction />} />
+            <Route path="/spatial" element={<Spatial />} />
+            <Route path="/beatwise" element={<BeatWiseAnalysis />} />
+            <Route path="/temporal" element={<TemporalAnalysis />} />
+            <Route path="/deployment" element={<Deployment />} />
             <Route path="/map" element={<Map />} />
           </Routes>
         </div>
---
+++ heatmap-frontend/src/components/TemporalAnalysis.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { MapContainer, TileLayer } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+
+const TemporalAnalysis = () => {
+    const defaultPosition = [14.5204, 75.7224]; // Latitude and Longitude of Karnataka
+
+    return (
+      <div className="relative w-full h-screen">
+        <MapContainer center={defaultPosition} zoom={13} className="h-full w-full">
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          />
+          {/* Additional map features go here */}
+        </MapContainer>
+      </div>
+    );
+};
+
+export default TemporalAnalysis;
