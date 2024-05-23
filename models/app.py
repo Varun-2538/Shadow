b1c86d3cce86ae52baddd945338caedff01f5966
Varun singh <942525@gmail.com>
2024-05-24 01:03:22 +0530

    changed back *

--- models/app.py
+++ models/app.py
@@ -15,7 +15,7 @@ app = FastAPI()
 # Enable CORS for all routes and domains
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://gallants-ksp-2.onrender.com"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
