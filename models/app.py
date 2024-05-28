8572f5a7863ca35cece0e40beae4a4f8dd1d6c68
Varun singh <942525@gmail.com>
2024-05-28 14:21:44 +0530

    port changes

--- models/app.py
+++ models/app.py
@@ -151,4 +151,5 @@ async def read_csv():
 # Run the FastAPI app in debug mode on port 8000
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000, log_level="debug")
+    port = int(os.environ.get("PORT", 8000))
+uvicorn.run(app, host="0.0.0.0", port=port, log_level="debug")
